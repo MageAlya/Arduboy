cb27d7a720f24684d529800e22c3a7bee9dd5546
MageAlya <alya.mansoor786@gmail.com>
2017-04-14 16:08:42 -0500

    Update Printing_Text.ino
    Organization

--- Printing_Text.ino
+++ Printing_Text.ino
@@ -1,18 +1,16 @@
 //MageAlya
 //April 14th, 2017
 //Printing Text
+
 #include <Arduboy.h>
 Arduboy arduboy;
+
 void setup() {
   // put your setup code here, to run once:
+  
 arduboy.begin();
 arduboy.clear();
 arduboy.print("Cubimals are the best!");
 arduboy.display();
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
 
 }
